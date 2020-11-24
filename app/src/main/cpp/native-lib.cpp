80be8c58dfb7ac6eb7e39d68fa74816f57b7c56c
shinplest <shinplest@gmail.com>
2020-11-24 11:27:56 +0900

    test

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <chrono>
 #include <arm_neon.h>
-#include <bitset>
+
 
 short *generateRamp(short startValue, short len) {
     short *ramp = new short[len];
@@ -12,20 +12,21 @@ short *generateRamp(short startValue, short len) {
     return ramp;
 }
 
+
+int dotProduct(short *vector1, short *vector2, short len) {
+    int result = 0;
+
+    for (short i = 0; i < len; i++) {
+        result += vector1[i] * vector2[i];
+    }
+    return result;
+}
+
 double msElapsedTime(std::chrono::system_clock::time_point start) {
     auto end = std::chrono::system_clock::now();
     return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 }
 
-std::chrono::system_clock::time_point now() {
-    return std::chrono::system_clock::now();
-}
-
-int dotProduct(short *vector1, short *vector2, short len) {
-    int result = 0;
-    for (short i = 0; i < len; i++) { result += vector1[i] * vector2[i]; }
-    return result;
-}
 
 int dotProductNeon(short *vector1, short *vector2, short len) {
     const short transferSize = 4;
@@ -40,10 +41,18 @@ int dotProductNeon(short *vector1, short *vector2, short len) {
     } // Store partial sums int partial
     int partialSums[transferSize];
     vst1q_s32(partialSums, partialSumsNeon);// Sum up partial sums
+
     int result = 0;
-    for (short i = 0; i < transferSize; i++) { result += partialSums[i]; }
+    for (short i = 0; i < transferSize; i++) {
+        result += partialSums[i];
+    }
     return result;
 }
+
+std::chrono::system_clock::time_point now() {
+    return std::chrono::system_clock::now();
+}
+
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_shinplest_neonintrinsic_MainActivity_stringFromJNI(
